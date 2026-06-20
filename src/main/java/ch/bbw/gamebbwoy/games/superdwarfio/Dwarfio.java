12fd316c2ae3fb4517f08d00ff57d235e006bae5
christian_gebert <christian.gebert@bbw.ch>
2026-06-20 13:53:31 +0200

    student's branch start

---
+++ src/main/java/ch/bbw/gamebbwoy/games/superdwarfio/Dwarfio.java
@@ -0,0 +1,17 @@
+package ch.bbw.gamebbwoy.games.superdwarfio;
+
+import ch.bbw.gamebbwoy.api.PixelDisplay;
+import ch.bbw.gamebbwoy.api.PixelDrawing;
+
+public class Dwarfio implements PixelDrawing {
+
+    @Override
+    public void initialize(PixelDisplay graphic) {
+        PixelDrawing.super.initialize(graphic);
+    }
+
+    @Override
+    public void tick(PixelDisplay graphic) {
+
+    }
+}
---
+++ src/main/java/ch/bbw/gamebbwoy/games/superdwarfio/Game.java
@@ -0,0 +1,9 @@
+package ch.bbw.gamebbwoy.games.superdwarfio;
+
+import ch.bbw.gamebbwoy.internal.GameBbwoy;
+
+public class Game {
+    public static void main(String[] args) throws Throwable {
+        GameBbwoy.playGame(new Dwarfio());
+    }
+}
